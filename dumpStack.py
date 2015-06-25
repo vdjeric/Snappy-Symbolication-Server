c7b26c9318168481bf4d2478ae1862df8d3a8b0c
Vladan Djeric <vladandjeric@gmail.com>
2015-06-24 18:32:36 -0700

    Added a tool to help with interactive debugging

---
+++ dumpStack.py
@@ -0,0 +1,20 @@
+# helper for debugging with pdb
+
+import sys
+import traceback
+
+def printThreads():
+    print >> sys.stderr, "\n*** STACKTRACE - START ***\n"
+    code = []
+    for threadId, stack in sys._current_frames().items():
+        code.append("\n# ThreadID: %s" % threadId)
+        for filename, lineno, name, line in traceback.extract_stack(stack):
+            code.append('File: "%s", line %d, in %s' % (filename,
+                                                        lineno, name))
+            if line:
+                code.append("  %s" % (line.strip()))
+    
+    for line in code:
+        print >> sys.stderr, line
+    print >> sys.stderr, "\n*** STACKTRACE - END ***\n"
+ 
